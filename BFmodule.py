dc347ce616f34627fa4b31de8d875bed53100219
lszucchi <lszucchi@gmail.com>
2024-10-09 14:40:09 -0300

    241009 working get post untested

--- BFmodule.py
+++ BFmodule.py
@@ -3,73 +3,60 @@ from DeviceParameters import DEVICE_IP, PORT, ApiKey
 
 VALID_TCHANNELS = ['t50k', 't4k', 'tmagnet', 'tstill', 'tmixing', 'tfse']
 
-class DR(self, DEVICE_IP, PORT, ApiKey, timeout=10):
+class DR:
 
-    def ____init____:
-        req=requests.get(f'https://{DEVICE_IP}:{PORT}/system?key={api_key}', timeout=timeout)
-        if req.status_code==200:]
+    def __init__(self, timeout=10):
+        global DEVICE_IP, PORT, ApiKey
+        req=requests.get(f'https://{DEVICE_IP}:{PORT}/system?key={ApiKey}', timeout=timeout)
+        if req.status_code==200:
             self.DEVICE_IP=DEVICE_IP
             self.PORT=PORT
             self.ApiKey=ApiKey
-            return req.json()['data']['system_name']
-        else raise f"Initial HTTP request failed. Status Code:{req.stats_code}"
+            self.timeout=timeout
+        else:
+            raise f"Initial HTTP request failed. Status Code:{req.stats_code}"
 
-        def Get(self, node):
-            req=requests.get(f"https://{self.DEVICE_IP}:{self.PORT}/{node}?key={self.api_key}", timeout=self.timeout)
-            return req
+    def Get(self, node):
+        req=requests.get(f"https://{self.DEVICE_IP}:{self.PORT}/{node}?key={self.ApiKey}", timeout=self.timeout)
+        return req
 
-        def Post(self, node, data):
-            req = requests.post((f"https://{self.DEVICE_IP}:{self.PORT}/{node}?key={self.api_key}", json=data, timeout=self.rimeout)
-            return req
+    def Post(self, node, data):
+        req = requests.post(f"https://{self.DEVICE_IP}:{self.PORT}/{node}?key={self.ApiKey}", json=data, timeout=self.rimeout)
+        return req
 
-        def GetT(self, name, fstr=None):
-            if name not in VALID_TCHANNELS:
-                return "Invalid channel"
-            
-            req=self.Get(f"values/mapper/bf/temperatures/{name}")
-            if req.status_code != 200:
-                return f"Comm failed. Status Code {req.status_code}"
+    def GetT(self, name, fstr=None):
+        if name not in VALID_TCHANNELS:
+            return "Invalid channel"
+        
+        req=self.Get(f"values/mapper/bf/temperatures/{name}")
+        if req.status_code != 200:
+            return f"Comm failed. Status Code {req.status_code}"
 
-            out=float(req.json()['data'][f'mapper.bf.temperatures.{name}']['content']['latest_value']['value'])
-            
-            try:
-                return format(out, fstr)
-            except:
-                return out
+        return req.json()['data'][f'mapper.bf.temperatures.{name}']['content']['latest_value']['value']
 
-        def GetP(self, Chn, fstr=None):
-            if Chn not in range(1, 7):
-                return "Invalid channel"
-            
-            req=self.Get(f"values/mapper/bf/pressures/p{name}")
-            if req.status_code != 200:
-                return f"Comm failed. Status Code {req.status_code}"
+    def GetP(self, Chn):
+        if Chn not in range(1, 7):
+            return "Invalid channel"
+        
+        req=self.Get(f"values/mapper/bf/pressures/p{Chn}")
+        if req.status_code != 200:
+            return f"Comm failed. Status Code {req.status_code}"
 
-            out=float(req.json()['data'][f'mapper.bf.pressures.p{name}']['content']['latest_value']['value'])
+        return req.json()['data'][f'mapper.bf.pressures.p{Chn}']['content']['latest_value']['value']
 
-            try:
-                return format(out, fstr)
-            except:
-                return out
+    def GetFlow(self):
+        req=self.Get(f"values/mapper/bf/flow")
+        if req.status_code != 200:
+            return f"Comm failed. Status Code {req.status_code}"
 
-        def GetFlow(self, fstr=None):
-            req=self.Get(f"values/mapper/bf/flow")
-            if req.status_code != 200:
-                return f"Comm failed. Status Code {req.status_code}"
+        return req.json()['data']['mapper.bf.flow']['content']['latest_value']['value']
 
-            out=float(req.json()['data']['mapper.bf.flow']['content']['latest_value']['value'])
+    def GetPulseTubeStatus(self):
+        req=self.Get(f"values/mapper/bf/pulsetube")
+        if req.status_code != 200:
+            return f"Comm failed. Status Code {req.status_code}"
 
-            try:
-                return format(out, fstr)
-            except:
-                return out
-
-        def GetPulseTubeStatus(self, fstr=None):
-            req=self.Get(f"values/mapper/bf/pulsetube")
-            if req.status_code != 200:
-                return f"Comm failed. Status Code {req.status_code}"
-
-            return int(req.json()['data']['mapper.bf.pulsetube']['content']['latest_value']['value'])
+        return int(req.json()['data']['mapper.bf.pulsetube']['content']['latest_value']['value'])
             
             
             
