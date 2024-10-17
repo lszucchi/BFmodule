a3c70e026d737753f8a3009a8853b39dc1c6b9bf
lszucchi <lszucchi@gmail.com>
2024-10-17 18:06:12 -0300

    GetTR & Log 241017

--- BFmodule.py
+++ BFmodule.py
@@ -1,12 +1,13 @@
-import requests, json
+import requests, json, sys
+sys.path.append('.')
 from DeviceParameters import DEVICE_IP, PORT, ApiKey
 
-TCHANNELS = ['t50k', 't4k', 'tmagnet', 'tstill', 'tmixing', 'tfse']
+TCHANNELS = ['', 't50k', 't4k', 'tmagnet', '', 'tstill', 'tmixing', '', 'tfse']
 PCHANNELS = ["can", "p2", "p3", "p4", "tank", "p6"]
 
 class DR:
 
-    def __init__(self, DEVICE_IP, PORT, ApiKey, timeout=10):
+    def __init__(self, timeout=10):
         req=requests.get(f'https://{DEVICE_IP}:{PORT}/system?key={ApiKey}', timeout=timeout)
         if req.status_code==200:
             self.DEVICE_IP=DEVICE_IP
@@ -46,7 +47,23 @@ class DR:
         return float('NaN')
 
     def GetTCh(self, Chn):
-        return self.GetT(TCHANNELS[Chn-1])
+        return self.GetT(TCHANNELS[Chn])
+
+    def GetTRCh(self, Chn):
+        req=self.Get(f"values/mapper/temperature_control/sensors/t{Chn}/resistance")
+        if req.status_code != 200:
+            return f"Comm failed. Status Code {req.status_code}"
+         
+        try:
+            out=float(req.json()['data'][f'mapper.temperature_control.sensors.t{Chn}.resistance']['content']['latest_value']['value'])
+            if out>0:
+                return out
+        except:
+            pass
+        return float('NaN') 
+        
+    def GetTR(self, name):
+        return self.GetTRCh(TCHANNELS.index(name))
 
     def GetPCh(self, Chn):
         if Chn not in range(1, 7):
---
+++ ElsaLogs.py
@@ -0,0 +1,54 @@
+import pandas as pd
+import numpy as np
+from datetime import datetime
+from time import sleep
+from os.path import isfile, expanduser
+from os import makedirs
+import sys
+from BFmodule import DR, TCHANNELS, PCHANNELS
+
+def WriteLog(msg, path, mode='a', end='\n', output=True):
+    makedirs(path.rsplit('/', 1)[0], exist_ok=True)
+    with open(path, mode) as logfile:
+        logfile.write(msg+end)
+    if output:
+        print(f'{msg}{end}', end='')
+
+def InitLog(path):
+    date=datetime.now().strftime('%y%m%d')
+    tlog=f"{path}/TLog {date}.csv"
+    plog=f"{path}/PLog {date}.csv"
+    slog=f"{path}/SLog {date}.csv"
+    if not isfile(tlog):
+        WriteLog(f"Time,{','.join(TCHANNELS)}", tlog, 'w')
+    if not isfile(plog):
+        WriteLog(f"Time,{','.join(PCHANNELS)}", plog, 'w')
+    return date, tlog, plog
+
+def Logging(path, period):
+    date=None
+    tlog=None
+    plog=None
+    Elsa=DR()
+    while(True):
+        while datetime.now().time().second%period:
+            sleep(0.5)
+        if date!=datetime.now().strftime('%y%m%d') or not isfile(plog) or not isfile(tlog):
+            date, tlog, plog=InitLog(path)
+        time=datetime.now().strftime('%H:%M:%S')
+        
+        WriteLog(f"{time}", tlog, end='')
+        for name in TCHANNELS:
+            if name:
+                temp=Elsa.GetT(name)
+                WriteLog(f",{format(temp, '07.3f') if temp > 0.1 else format(temp*1e3, '06.3f')+'e-3' if not np.isnan(temp) else np.nan}", tlog, end='')
+        WriteLog('', tlog)
+        
+        WriteLog(f"{time},{','.join([format(Elsa.GetPCh(k), '.3e') for k in range(1, 7)])},{format(Elsa.GetFlow(), '.2f')}", plog)
+        sleep(1)
+    return 1
+
+        
+Logging(expanduser('~/Documents/ElsaLogs'), 60)
+
+    
